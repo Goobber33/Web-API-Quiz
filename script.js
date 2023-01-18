390667c6e3d3f65212c2d2376e217340331361cc
Kyle Parks <kyleparks@Kyles-MacBook-Pro.local>
2023-01-18 09:19:56 -0800

    Moved the timer to the right side of the page.

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
+// This syntax was taken from the UW Gitlab repository, on Web-APIs - Day 01 - 09-Ins-Timers-Intervals
+
 var timeEl = document.querySelector(".time");
 var secondsLeft = 60;
 
 function setTime() {
-  // Sets interval in variable
-  var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timeEl.textContent = secondsLeft + "";
 
-    if(secondsLeft === 0) {
-      clearInterval(timerInterval)
-      sendMessage();
-    }
+    var timerInterval = setInterval(function () {
+        secondsLeft--;
+        timeEl.textContent = secondsLeft + "";
 
-  }, 1000);
+        if (secondsLeft === 0) {
+            clearInterval(timerInterval)
+            sendMessage();
+        }
+
+    }, 1000);
 }
 
 setTime();
