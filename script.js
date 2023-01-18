27ba6d7763c993d85005f8098601e8657ac0b8ed
Kyle Parks <kyleparks@Kyles-MacBook-Pro.local>
2023-01-18 13:44:03 -0800

    Added first multiple choice question, and titles the variable multiQuestions

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
+// I need to write questions, multiple choice answers, and the correct answer
+
+
+// Create an array?
+var multiQuestions = [
+    {
+        question: 'Which of the following methods is used to access HTML elements using Javascript:',
+        multipleChoices: ['getElementbyId()', 'getElementsByClassName()', 'Both A and B', 'None of the above'],
+        answer: 'Both A and B'
+    },
+];
+
 // This syntax was taken from the UW Gitlab repository, on Web-APIs - Day 01 - 09-Ins-Timers-Intervals
 
 var timeEl = document.querySelector(".time");
@@ -11,10 +23,24 @@ function setTime() {
 
         if (secondsLeft === 0) {
             clearInterval(timerInterval)
-            sendMessage();
+            sendMessage('Time is up');
         }
 
     }, 1000);
 }
 
 setTime();
+
+
+
+
+
+
+
+
+
+
+
+// Questions
+
+// For this project and in general, would it be better to style in CSS or JS (Example: h1El.setAttribute("style", "margin:auto; width:50%; text-align:center;");)
