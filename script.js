66da9d1f8f68fbfcae5571bf9c16b27a48233b63
Kyle Parks <kyleparks@Kyles-MacBook-Pro.local>
2023-01-18 14:11:36 -0800

    Added questions 4 and 5.

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var multiQuestions = [
         answer: 'Both A and B'
     },
 
-// Question 2
+    // Question 2
 
     {
         question: 'When an operators value is NULL, the typeof returned by the unary operator is:',
@@ -28,6 +28,21 @@ var multiQuestions = [
         answer: 'Object-Oriented'
     },
 
+    // Question 4
+
+    {
+        question: 'Which of the following keywords is used to define a variable in Javascript?',
+        multipleChoices: ['var', 'let', 'Both A and B', 'None of the above'],
+        answer: 'Both A and B'
+    },
+
+    // Question 5
+
+    {
+        question: 'Which of the following methods can be used to display data in some form using Javascript?',
+        multipleChoices: ['document.write', 'console.log()', 'window.alert', 'All of the above'],
+        answer: 'All of the above'
+    },
 ];
 
 // This syntax was taken from the UW Gitlab repository, on Web-APIs - Day 01 - 09-Ins-Timers-Intervals
